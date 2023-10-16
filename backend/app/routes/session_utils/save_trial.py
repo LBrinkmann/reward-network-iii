6ef7bd263138674884e98ab6984ec3b24f744bf8
levin.brinkmann01 <brinkmann@mpib-berlin.mpg.de>
2023-10-16 10:23:33 +0200

    Add documentation to trials.

--- backend/app/routes/session_utils/save_trial.py
+++ backend/app/routes/session_utils/save_trial.py
@@ -4,39 +4,48 @@ from typing import List
 from beanie import PydanticObjectId
 
 from models.session import Session
-from models.trial import Trial, Solution, TrialError, WrittenStrategy, \
-    Advisor, PostSurvey
+from models.trial import (
+    Trial,
+    Solution,
+    TrialError,
+    WrittenStrategy,
+    Advisor,
+    PostSurvey,
+)
 from utils.utils import estimate_solution_score
 
 
 async def save_trial(body, session, trial, trial_type):
     # save trial results
-    if trial_type == 'individual':
+    if trial_type == "individual":
         save_individual_demonstration_trial(trial, body)
-    elif trial_type == 'social_learning_selection':
+    elif trial_type == "social_learning_selection":
         # select all social learning trials for one advisor
+        # TODO: simply by adding a block id to trials, and selecting here all
+        # trails from block
+        # TODO: move all this logic to "save_social_learning_selection"
         sl_start = session.current_trial_num
         sl_end = 1
-        for t in session.trials[sl_start + 1:]:
-            if t.trial_type in ['social_learning_selection', 'individual']:
+        for t in session.trials[sl_start + 1 :]:
+            if t.trial_type in ["social_learning_selection", "individual"]:
                 break
             sl_end += 1
-        trials = session.trials[sl_start:sl_start + sl_end]
+        trials = session.trials[sl_start : sl_start + sl_end]
         await save_social_learning_selection(trials, session.subject_id, body)
-        session.trials[sl_start:sl_start + sl_end] = trials
-    elif trial_type == 'observation':
+        session.trials[sl_start : sl_start + sl_end] = trials
+    elif trial_type == "observation":
         save_individual_demonstration_trial(trial, body)
-    elif trial_type == 'repeat':
+    elif trial_type == "repeat":
         save_individual_demonstration_trial(trial, body)
-    elif trial_type == 'try_yourself':
+    elif trial_type == "try_yourself":
         save_individual_demonstration_trial(trial, body)
-    elif trial_type == 'demonstration':
+    elif trial_type == "demonstration":
         save_individual_demonstration_trial(trial, body)
-    elif trial_type == 'written_strategy':
+    elif trial_type == "written_strategy":
         save_written_strategy(trial, body)
-    elif trial_type == 'post_survey':
+    elif trial_type == "post_survey":
         save_survey_trial(trial, body)
-    elif trial_type in ['consent', 'practice', 'debriefing', 'instruction']:
+    elif trial_type in ["consent", "practice", "debriefing", "instruction"]:
         save_empty_trial(trial)
 
     # update session with the trial
@@ -45,13 +54,13 @@ async def save_trial(body, session, trial, trial_type):
 
 def save_individual_demonstration_trial(trial: Trial, body: Solution):
     if not isinstance(body, Solution):
-        return TrialError(message='Trial results are missing')
+        return TrialError(message="Trial results are missing")
 
     trial.solution = Solution(
         moves=body.moves,
         score=estimate_solution_score(trial.network, body.moves),
         trial_id=trial.id,
-        finished_at=datetime.now()
+        finished_at=datetime.now(),
     )
     trial.finished_at = datetime.now()
     trial.finished = True
@@ -59,12 +68,10 @@ def save_individual_demonstration_trial(trial: Trial, body: Solution):
 
 def save_written_strategy(trial: Trial, body: WrittenStrategy):
     if not isinstance(body, WrittenStrategy):
-        return TrialError(message='Trial results are missing')
+        return TrialError(message="Trial results are missing")
 
     trial.written_strategy = WrittenStrategy(
-        strategy=body.strategy,
-        trial_id=trial.id,
-        finished_at=datetime.now()
+        strategy=body.strategy, trial_id=trial.id, finished_at=datetime.now()
     )
     trial.finished_at = datetime.now()
     trial.finished = True
@@ -72,42 +79,46 @@ def save_written_strategy(trial: Trial, body: WrittenStrategy):
 
 def save_survey_trial(trial: Trial, body: PostSurvey):
     if not isinstance(body, PostSurvey):
-        return TrialError(message='Trial results are missing')
+        return TrialError(message="Trial results are missing")
 
     trial.post_survey = PostSurvey(
-        questions=body.questions,
-        trial_id=trial.id,
-        finished_at=datetime.now()
+        questions=body.questions, trial_id=trial.id, finished_at=datetime.now()
     )
     trial.finished_at = datetime.now()
     trial.finished = True
 
 
-async def save_social_learning_selection(trials: List[Trial],
-                                         subject_id: PydanticObjectId,
-                                         body: Advisor):
+async def save_social_learning_selection(
+    trials: List[Trial], subject_id: PydanticObjectId, body: Advisor
+):
     if not isinstance(body, Advisor):
-        return TrialError(message='Trial results are missing')
+        return TrialError(message="Trial results are missing")
 
     sl_selection = trials[0]
     sl_trials = trials[1:]
 
     # remove instruction trial if it is in the list of social learning trials
-    sl_trials = [t for t in sl_trials if t.trial_type != 'instruction']
+    sl_trials = [t for t in sl_trials if t.trial_type != "instruction"]
 
     # get advisor session
     ad_s = await Session.get(body.advisor_id)
 
     if ad_s is None:
-        return TrialError(message='Advisor session is not found')
+        return TrialError(message="Advisor session is not found")
 
     # get advisor demonstration trials
-    ad_trials = [t for t in ad_s.trials if t.trial_type == 'individual']
+    # TODO: It looks like we are here not taking the demonstration trails;
+    # This suggest that demonstration trails are not used anymore
+    # Consider removing demonstration trails all together
+    ad_trials = [t for t in ad_s.trials if t.trial_type == "individual"]
 
     # select advisor's written strategy
-    wr_s = [t.written_strategy for t in ad_s.trials if
-            t.trial_type == 'written_strategy'][0]
+    wr_s = [
+        t.written_strategy for t in ad_s.trials if t.trial_type == "written_strategy"
+    ][0]
 
+    # TODO: refactor advisor trail selection; we want now to take the last x
+    # trails from the advisors demonstration trails
     # iterate over advisor's demonstration trials
     for n, t in enumerate(ad_trials):
         # update `selected_by_children` field for advisor's demonstration trial
@@ -124,8 +135,7 @@ async def save_social_learning_selection(trials: List[Trial],
             sl_trials[n * 3 + i].network = t.network
 
     sl_selection.advisor = Advisor(
-        advisor_id=body.advisor_id,
-        written_strategy=wr_s.strategy
+        advisor_id=body.advisor_id, written_strategy=wr_s.strategy
     )
 
     sl_selection.finished_at = datetime.now()
