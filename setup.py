12ce2cf78ab338cb1ed07e9cb0fd6a05b49edd9b
Levin <levin.brinkmann@gmail.com>
2023-10-20 19:13:52 +0200

    Add common setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+def read_requirements(file):
+    with open(file, "r") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+
+setup(
+    name="common",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=read_requirements("requirements.txt"),
+    extras_require={
+        "dev": read_requirements("requirements_dev.txt"),
+        "viz": read_requirements("requirements_viz.txt"),
+        "backend": read_requirements("requirements_backend.txt"),
+        "train": read_requirements("requirements_train.txt"),
+    },
+)
