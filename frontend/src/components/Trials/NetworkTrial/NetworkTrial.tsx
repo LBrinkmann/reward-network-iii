2171c782d0989c13005187952f4287a68ab05171
levin.brinkmann01 <brinkmann@mpib-berlin.mpg.de>
2023-11-21 00:43:20 +0100

    Update text.

--- frontend/src/components/Trials/NetworkTrial/NetworkTrial.tsx
+++ frontend/src/components/Trials/NetworkTrial/NetworkTrial.tsx
@@ -1,201 +1,259 @@
-import React, {FC, useEffect} from "react";
+import React, { FC, useEffect } from "react";
 import useNetworkContext from "../../../contexts/NetworkContext";
-import {Box, Divider, Grid, Typography} from "@mui/material";
-import StaticNetwork, {StaticNetworkEdgeInterface} from "../../Network/StaticNetwork/StaticNetwork";
+import { Box, Divider, Grid, Typography } from "@mui/material";
+import StaticNetwork, {
+  StaticNetworkEdgeInterface,
+} from "../../Network/StaticNetwork/StaticNetwork";
 import PlayerInformation from "../PlayerInformation";
 import LinearSolution from "../../Network/LinearSolution";
 import Timer from "../../Timer";
-import {NETWORK_ACTIONS} from "../../../reducers/NetworkReducer";
+import { NETWORK_ACTIONS } from "../../../reducers/NetworkReducer";
 import Legend from "./RewardsLegend";
 
 interface NetworkTrialInterface {
-    showLegend?: boolean;
-    showComment?: boolean;
-    teacherId?: number;
-    showLinearNetwork?: boolean;
-    showTimer?: boolean;
-    time?: number;
-    isPractice?: boolean;
-    isTimerPaused?: boolean;
-    playerTotalPoints?: number;
-    showCurrentNetworkPoints?: boolean;
-    showTotalPoints?: boolean;
-    allowNodeClick?: boolean;
+  showLegend?: boolean;
+  showComment?: boolean;
+  teacherId?: number;
+  showLinearNetwork?: boolean;
+  showTimer?: boolean;
+  time?: number;
+  isPractice?: boolean;
+  isTimerPaused?: boolean;
+  playerTotalPoints?: number;
+  showCurrentNetworkPoints?: boolean;
+  showTotalPoints?: boolean;
+  allowNodeClick?: boolean;
 }
 
 const NetworkTrial: FC<NetworkTrialInterface> = (props) => {
-    const {
-        showLegend = true,
-        showComment = false,
-        teacherId = 1,
-        showLinearNetwork = true,
-        showTimer = true,
-        time = 35,
-        isPractice = false,
-        isTimerPaused = false,
-        playerTotalPoints = 0,
-        showCurrentNetworkPoints = true,
-        showTotalPoints = true,
-        allowNodeClick = true
-    } = props;
-    const {networkState, networkDispatcher} = useNetworkContext();
+  const {
+    showLegend = true,
+    showComment = false,
+    teacherId = 1,
+    showLinearNetwork = true,
+    showTimer = true,
+    time = 35,
+    isPractice = false,
+    isTimerPaused = false,
+    playerTotalPoints = 0,
+    showCurrentNetworkPoints = true,
+    showTotalPoints = true,
+    allowNodeClick = true,
+  } = props;
+  const { networkState, networkDispatcher } = useNetworkContext();
 
-    const NodeClickHandler = (nodeIdx: number) => {
-        if (!allowNodeClick) return;
+  const NodeClickHandler = (nodeIdx: number) => {
+    if (!allowNodeClick) return;
 
-        // skip update if network is disabled or finished
-        if (networkState.isNetworkDisabled || networkState.isNetworkFinished) return;
+    // skip update if network is disabled or finished
+    if (networkState.isNetworkDisabled || networkState.isNetworkFinished)
+      return;
 
-        // allow clicking only for some tutorial steps
-        if (isPractice && !(networkState.tutorialOptions.edge ||
-            networkState.tutorialOptions.linearSolution)) return;
-
-        networkDispatcher({type: NETWORK_ACTIONS.NEXT_NODE, payload: {nodeIdx}});
-        if (isPractice) networkDispatcher({type: NETWORK_ACTIONS.NEXT_TUTORIAL_STEP});
-    }
-
-    const NextTutorialStepHandler = () => networkDispatcher({type: NETWORK_ACTIONS.NEXT_TUTORIAL_STEP,});
-
-    const onTutorialCommentClose = () => networkDispatcher({type: NETWORK_ACTIONS.FINISH_COMMENT_TUTORIAL,});
-
-
-    return (
-        <Grid container sx={{margin: 'auto', width: '85%'}} justifyContent="space-around">
-            <Grid item sx={{p: 1}} xs={3}>
-                <Grid container direction="column">
-                    <Grid item xs={3} mt={"340px"}>
-                        {showTimer &&
-                            <Timer
-                                time={time}
-                                invisibleTime={5} // 5 seconds before the timer starts
-                                pause={isTimerPaused || networkState.isNetworkFinished || networkState.isNetworkDisabled}
-                                showTutorial={networkState.tutorialOptions.time}
-                                onTutorialClose={NextTutorialStepHandler}
-                            />
-                        }
-                    </Grid>
-                    <Grid item xs={8}>
-                        <PlayerInformation
-                            id={teacherId}
-                            step={networkState.step}
-                            cumulativePoints={networkState.points}
-                            totalScore={playerTotalPoints + networkState.points}
-                            showComment={showComment}
-                            comment={networkState.teacherComment}
-                            showTutorialScore={networkState.tutorialOptions.points}
-                            showTutorialComment={networkState.tutorialOptions.comment}
-                            onTutorialCommentClose={onTutorialCommentClose}
-                            showTutorialTotalScore={networkState.tutorialOptions.totalScore}
-                            onTutorialClose={NextTutorialStepHandler}
-                            showCumulativePoints={showCurrentNetworkPoints}
-                            showTotalPoints={showTotalPoints}
-                        />
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid item xs={6}>
-                <Grid container direction="column" justifyContent="space-around">
-                    <Grid item style={{position: 'relative'}}>
-                        <FlashingReward/>
-                        <StaticNetwork
-                            edges={networkState.network.edges.filter(
-                                (edge: StaticNetworkEdgeInterface) => networkState.moves.includes(edge.source_num)
-                            )}
-                            nodes={networkState.network.nodes}
-                            currentNodeId={networkState.isNetworkFinished ? null : networkState.currentNode}
-                            possibleMoves={networkState.possibleMoves}
-                            onNodeClickHandler={NodeClickHandler}
-                            disableClick={networkState.isNetworkDisabled}
-                            showEdgeTutorial={networkState.tutorialOptions.edge}
-                            showNodeTutorial={networkState.tutorialOptions.node}
-                            onTutorialClose={NextTutorialStepHandler}
-                            blur={networkState.tutorialOptions.comment}
-                        />
-                    </Grid>
-                    <Grid item sx={{marginTop: '10px'}}>
-                        {showLinearNetwork &&
-                            <LinearSolution
-                                edges={networkState.network.edges}
-                                nodes={networkState.network.nodes}
-                                moves={networkState.moves}
-                                showTutorial={networkState.tutorialOptions.linearSolution}
-                            />
-                        }
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid item xs={1}>
-                <Grid container direction="column" justifyContent="center" alignItems="center">
-                    {showLegend &&
-                        <Grid item mt={"150px"}>
-                            <Legend/>
-                        </Grid>}
-                </Grid>
-            </Grid>
-        </Grid>
-    );
-}
-const FlashingReward: FC = () => {
-    const allRewards = [-50, 0, 100, 200, 400];
-    const colors = ['#c51b7d', '#e9a3c9', '#e6f5d0', '#a1d76a', '#4d9221',];
-    const {networkState} = useNetworkContext();
-    const [show, setShow] = React.useState(false);
-    useEffect(() => {
-        if (networkState.rewardIdx !== 0) {
-            setShow(true);
-        }
-    }, [networkState.rewardIdx]);
-
-    useEffect(() => {
-        if (show) {
-            // set timeout to reset status
-            setTimeout(() => {
-                setShow(false);
-            }, 800);
-        }
-    }, [show]);
-
-
-    var color;
-    var text;
-    if (networkState.currentReward === undefined) {
-        color = 'white'
-        text = ''
-    } else if (networkState.forceSolution) {
-        color = networkState.currentReward > 0 ? colors[2] : colors[0]
-        text = networkState.currentReward > 0 ? 'Correct' : 'Wrong'
-    } else {
-        color = colors[allRewards.indexOf(networkState.currentReward)]
-        text = networkState.currentReward > 0 ? 'Gain' : (networkState.currentReward < 0 ? 'Loss' : 'Neutral')
-    }
-
-    return (
-        <Box
-            style={{position: 'absolute', top: '160px', left: '160px', opacity: show ? 0.8 : 0}}
-            sx={{
-                borderRadius: '50%',
-                width: '60px',
-                height: '40px',
-                bgcolor: color,
-                // typography: 'h4',
-                // textAlign: 'center',
-                px: "40px",
-                py: "50px",
-                display: 'flex',              // Use flexbox for alignment
-                flexDirection: 'column',      // Stack child elements vertically
-                justifyContent: 'center',     // Center child elements vertically
-                alignItems: 'center',         // Center child elements horizontally
-            }}
-        >
-            <Typography variant="h4" align="center">
-                {text}
-            </Typography>
-            <Typography variant="h4" align="center">
-                {networkState.currentReward}
-            </Typography>
-        </Box>
+    // allow clicking only for some tutorial steps
+    if (
+      isPractice &&
+      !(
+        networkState.tutorialOptions.edge ||
+        networkState.tutorialOptions.linearSolution
+      )
     )
-}
+      return;
 
+    networkDispatcher({
+      type: NETWORK_ACTIONS.NEXT_NODE,
+      payload: { nodeIdx },
+    });
+    if (isPractice)
+      networkDispatcher({ type: NETWORK_ACTIONS.NEXT_TUTORIAL_STEP });
+  };
+
+  const NextTutorialStepHandler = () =>
+    networkDispatcher({ type: NETWORK_ACTIONS.NEXT_TUTORIAL_STEP });
+
+  const onTutorialCommentClose = () =>
+    networkDispatcher({ type: NETWORK_ACTIONS.FINISH_COMMENT_TUTORIAL });
+
+  return (
+    <Grid
+      container
+      sx={{ margin: "auto", width: "85%" }}
+      justifyContent="space-around"
+    >
+      <Grid item sx={{ p: 1 }} xs={2}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="flex-start"
+          style={{ height: "250px" }}
+        >
+          <Grid item mt={"50px"} xs={12}>
+            {showTimer && (
+              <Timer
+                time={time}
+                invisibleTime={5} // 5 seconds before the timer starts
+                pause={
+                  isTimerPaused ||
+                  networkState.isNetworkFinished ||
+                  networkState.isNetworkDisabled
+                }
+                showTutorial={networkState.tutorialOptions.time}
+                onTutorialClose={NextTutorialStepHandler}
+              />
+            )}
+          </Grid>
+          <Grid item xs={12}>
+            <PlayerInformation
+              id={teacherId}
+              step={networkState.step}
+              cumulativePoints={networkState.points}
+              totalScore={playerTotalPoints + networkState.points}
+              showComment={showComment}
+              comment={networkState.teacherComment}
+              showTutorialScore={networkState.tutorialOptions.points}
+              showTutorialComment={networkState.tutorialOptions.comment}
+              onTutorialCommentClose={onTutorialCommentClose}
+              showTutorialTotalScore={networkState.tutorialOptions.totalScore}
+              onTutorialClose={NextTutorialStepHandler}
+              showCumulativePoints={showCurrentNetworkPoints}
+              showTotalPoints={showTotalPoints}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={6}>
+        <Grid
+          container
+          direction="column"
+          justifyContent="space-around"
+          alignItems="center"
+        >
+          <Grid
+            item
+            style={{
+              position: "relative",
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <FlashingReward />
+            <StaticNetwork
+              edges={networkState.network.edges.filter(
+                (edge: StaticNetworkEdgeInterface) =>
+                  networkState.moves.includes(edge.source_num)
+              )}
+              nodes={networkState.network.nodes}
+              currentNodeId={
+                networkState.isNetworkFinished ? null : networkState.currentNode
+              }
+              possibleMoves={networkState.possibleMoves}
+              onNodeClickHandler={NodeClickHandler}
+              disableClick={networkState.isNetworkDisabled}
+              showEdgeTutorial={networkState.tutorialOptions.edge}
+              showNodeTutorial={networkState.tutorialOptions.node}
+              onTutorialClose={NextTutorialStepHandler}
+              blur={networkState.tutorialOptions.comment}
+            />
+          </Grid>
+          <Grid item sx={{ marginTop: "10px", width: "100%" }}>
+            {showLinearNetwork && (
+              <LinearSolution
+                edges={networkState.network.edges}
+                nodes={networkState.network.nodes}
+                moves={networkState.moves}
+                showTutorial={networkState.tutorialOptions.linearSolution}
+              />
+            )}
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={2}>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          {showLegend && (
+            <Grid item mt={"200px"}>
+              <Legend />
+            </Grid>
+          )}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+const FlashingReward: FC = () => {
+  const allRewards = [-50, 0, 100, 200, 400];
+  const colors = ["#c51b7d", "#e9a3c9", "#e6f5d0", "#a1d76a", "#4d9221"];
+  const { networkState } = useNetworkContext();
+  const [show, setShow] = React.useState(false);
+  useEffect(() => {
+    if (networkState.rewardIdx !== 0) {
+      setShow(true);
+    }
+  }, [networkState.rewardIdx]);
+
+  useEffect(() => {
+    if (show) {
+      // set timeout to reset status
+      setTimeout(() => {
+        setShow(false);
+      }, 800);
+    }
+  }, [show]);
+
+  var color;
+  var text;
+  if (networkState.currentReward === undefined) {
+    color = "white";
+    text = "";
+  } else if (networkState.forceSolution) {
+    color = networkState.currentReward > 0 ? colors[2] : colors[0];
+    text = networkState.currentReward > 0 ? "Correct" : "Wrong";
+  } else {
+    color = colors[allRewards.indexOf(networkState.currentReward)];
+    text =
+      networkState.currentReward > 0
+        ? "Gain"
+        : networkState.currentReward < 0
+        ? "Loss"
+        : "Neutral";
+  }
+
+  return (
+    <Box
+      style={{
+        position: "absolute", // Position FlashingReward absolutely within the relative Grid item
+        top: "50%", // Center vertically
+        left: "50%", // Center horizontally
+        transform: "translate(-50%, -50%)", // Offset by half the width and height of FlashingReward
+        opacity: show ? 0.8 : 0,
+      }}
+      sx={{
+        borderRadius: "50%",
+        width: "60px",
+        height: "40px",
+        bgcolor: color,
+        // typography: 'h4',
+        // textAlign: 'center',
+        px: "40px",
+        py: "50px",
+        display: "flex", // Use flexbox for alignment
+        flexDirection: "column", // Stack child elements vertically
+        justifyContent: "center", // Center child elements vertically
+        alignItems: "center", // Center child elements horizontally
+      }}
+    >
+      <Typography variant="h4" align="center">
+        {text}
+      </Typography>
+      <Typography variant="h4" align="center">
+        {networkState.currentReward}
+      </Typography>
+    </Box>
+  );
+};
 
 export default NetworkTrial;
--- frontend/src/components/Trials/NetworkTrial/RewardsLegend.tsx
+++ frontend/src/components/Trials/NetworkTrial/RewardsLegend.tsx
@@ -13,7 +13,7 @@ interface ILegend {
 const Legend: FC<ILegend> = (props) => {
     const {
         showLegend = true,
-        size = {width: 100, height: 120},
+        size = {width: 150, height: 180},
         colors = ['#c51b7d', '#e9a3c9', '#e6f5d0', '#a1d76a', '#4d9221',],
         rewards = [-50, 0, 100, 200, 400]
     } = props;
@@ -33,15 +33,15 @@ const Legend: FC<ILegend> = (props) => {
                                     showRewardText={false}
                                     arc_type={'straight'}
                                     source_x={0}
-                                    source_y={idx * 20 + 15}
+                                    source_y={idx * 30 + 15}
                                     arc_x={20}
                                     arc_y={idx * 20 + 15}
                                     target_x={30}
-                                    target_y={idx * 20 + 15}
+                                    target_y={idx * 30 + 15}
                                     key={'legend-edge-' + idx}
 
                                 />
-                                <text x={50} y={idx * 20 + 20} fontSize={18} textAnchor="middle">{rewards[idx]}</text>
+                                <text x={60} y={idx * 30 + 20} fontSize={20} textAnchor="middle">{rewards[idx]}</text>
                             </React.Fragment>
                         )
                     })
