981f2269afef821b0cec5ddfdabd523355b28ec5
levin.brinkmann01 <brinkmann@mpib-berlin.mpg.de>
2023-11-28 00:48:24 +0100

    Update sl.

--- frontend/src/contexts/NetworkContext.tsx
+++ frontend/src/contexts/NetworkContext.tsx
@@ -1,126 +1,148 @@
-import React, {createContext, FC, useContext, useEffect, useReducer} from "react";
+import React, {
+  createContext,
+  FC,
+  useContext,
+  useEffect,
+  useReducer,
+} from "react";
 import {
-    StaticNetworkEdgeInterface,
-    StaticNetworkNodeInterface
+  StaticNetworkEdgeInterface,
+  StaticNetworkNodeInterface,
 } from "../components/Network/StaticNetwork/StaticNetwork";
 import networkReducer from "../reducers/NetworkReducer";
 
-const LOCAL_STORAGE_NETWORK_STATE_KEY = 'networkState';
-
+const LOCAL_STORAGE_NETWORK_STATE_KEY = "networkState";
 
 type timerState = {
-    timePassed: number;
-    isTimerPaused: boolean;
-    isTimerDone: boolean;
-
-}
-
-
-export type NetworkState = {
-    network: { edges: StaticNetworkEdgeInterface[], nodes: StaticNetworkNodeInterface[] } | undefined;
-    solution: {moves: number[], score?: number} | undefined;
-    step: number;
-    points: number;
-    moves: number[];
-    timer: timerState;
-    currentNode: number;
-    possibleMoves: number[];
-    allowedMoves: number[];
-    isNetworkDisabled: boolean;
-    isNetworkFinished: boolean;
-    isPractice: boolean;
-    tutorialStep: number;
-    tutorialOptions: {
-        start: boolean;
-        node: boolean;
-        general_edge: boolean;
-        edge: boolean,
-        general_points: boolean,
-        linearSolution: boolean,
-        time: boolean,
-        points: boolean,
-        totalScore: boolean,
-        comment: boolean,
-    };
-    teacherComment: string;
-    currentReward?: number;
-    rewardIdx?: number;
-    wrongRepeatPunishment: number;
-    correctRepeatReward: number;
-    forceSolution: boolean;
-}
-
-export type NetworkContextType = {
-    networkState: NetworkState | null;
-    networkDispatcher: (action: any) => void;
+  timePassed: number;
+  isTimerPaused: boolean;
+  isTimerDone: boolean;
 };
 
+export type NetworkState = {
+  network:
+    | {
+        edges: StaticNetworkEdgeInterface[];
+        nodes: StaticNetworkNodeInterface[];
+      }
+    | undefined;
+  solution: { moves: number[]; score?: number } | undefined;
+  step: number;
+  points: number;
+  moves: number[];
+  timer: timerState;
+  currentNode: number;
+  possibleMoves: number[];
+  allowedMoves: number[];
+  correctRepeats: boolean[];
+  wrongRepeat: boolean;
+  isNetworkDisabled: boolean;
+  isNetworkFinished: boolean;
+  isPractice: boolean;
+  tutorialStep: number;
+  tutorialOptions: {
+    start: boolean;
+    node: boolean;
+    general_edge: boolean;
+    edge: boolean;
+    general_points: boolean;
+    linearSolution: boolean;
+    time: boolean;
+    points: boolean;
+    totalScore: boolean;
+    comment: boolean;
+  };
+  teacherComment: string;
+  currentReward?: number;
+  rewardIdx?: number;
+  wrongRepeatPunishment: number;
+  correctRepeatReward: number;
+  trialType: string;
+};
+
+export type NetworkContextType = {
+  networkState: NetworkState | null;
+  networkDispatcher: (action: any) => void;
+};
 
 const NetworkContext = createContext<NetworkContextType | null>(null);
 
 export const networkInitialState: NetworkState = {
-    step: 0,
-    points: 0,
-    moves: [],
-    timer: {
-        timePassed: 0,
-        isTimerPaused: false,
-        isTimerDone: false,
-    },
-    currentNode: undefined,
-    possibleMoves: undefined,
-    allowedMoves: undefined,
-    isNetworkDisabled: false,
-    network: undefined,
-    solution: undefined,
-    isNetworkFinished: false,
-    isPractice: false,
-    tutorialStep: 0,
-    tutorialOptions: {
-        start: false,  // 1
-        node: false,  // 2
-        general_edge: false, // 3
-        edge: false,  // 4
-        general_points: false,  // 5
-        linearSolution: false,  // 6
-        time: false,  // 7
-        points: false,  // 8
-        totalScore: false,  // 9
-        comment: false,
-    },
-    teacherComment: '',
-    currentReward: undefined,
-    rewardIdx: 0,
-    wrongRepeatPunishment: 0,
-    correctRepeatReward: 0,
-    forceSolution: false,
-}
+  step: 0,
+  points: 0,
+  moves: [],
+  correctRepeats: [],
+  timer: {
+    timePassed: 0,
+    isTimerPaused: false,
+    isTimerDone: false,
+  },
+  currentNode: undefined,
+  wrongRepeat: false,
+  possibleMoves: undefined,
+  allowedMoves: undefined,
+  isNetworkDisabled: false,
+  network: undefined,
+  solution: undefined,
+  isNetworkFinished: false,
+  isPractice: false,
+  tutorialStep: 0,
+  tutorialOptions: {
+    start: false, // 1
+    node: false, // 2
+    general_edge: false, // 3
+    edge: false, // 4
+    general_points: false, // 5
+    linearSolution: false, // 6
+    time: false, // 7
+    points: false, // 8
+    totalScore: false, // 9
+    comment: false,
+  },
+  teacherComment: "",
+  currentReward: undefined,
+  rewardIdx: 0,
+  wrongRepeatPunishment: 0,
+  correctRepeatReward: 0,
+  trialType: "",
+};
 
 const networkInitializer = (initialState: NetworkState) => {
-    return JSON.parse(localStorage.getItem(LOCAL_STORAGE_NETWORK_STATE_KEY)) || initialState;
-}
+  return (
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_NETWORK_STATE_KEY)) ||
+    initialState
+  );
+};
 
 interface INetworkContextProvider {
-    children: any;
-    saveToLocalStorage?: boolean;
+  children: any;
+  saveToLocalStorage?: boolean;
 }
 
 export const NetworkContextProvider: FC<INetworkContextProvider> = (props) => {
-    const {children, saveToLocalStorage=true} = props;
-    const [state, dispatch] = useReducer(networkReducer, networkInitialState, networkInitializer);
+  const { children, saveToLocalStorage = true } = props;
+  const [state, dispatch] = useReducer(
+    networkReducer,
+    networkInitialState,
+    networkInitializer
+  );
 
-    useEffect(() => {
-        if(saveToLocalStorage)
-            localStorage.setItem(LOCAL_STORAGE_NETWORK_STATE_KEY, JSON.stringify(state));
-    }, [state]);
+  useEffect(() => {
+    if (saveToLocalStorage)
+      localStorage.setItem(
+        LOCAL_STORAGE_NETWORK_STATE_KEY,
+        JSON.stringify(state)
+      );
+  }, [state]);
 
-    return (
-        <NetworkContext.Provider value={{networkState: state, networkDispatcher: dispatch}}>
-            {children}
-        </NetworkContext.Provider>
-    );
+  return (
+    <NetworkContext.Provider
+      value={{ networkState: state, networkDispatcher: dispatch }}
+    >
+      {children}
+    </NetworkContext.Provider>
+  );
 };
-
 
 const useNetworkContext = () => useContext(NetworkContext);
 
