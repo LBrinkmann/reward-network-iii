1de3fbf0cc2c0efe75cbe8020c28d99df22c382f
Jaeeun Shin <shin@mpib-berlin.mpg.de>
2024-02-05 14:51:31 +0100

    Change instructions for demo phase

--- frontend/src/components/Trials/Instruction/InstructionContent.ts
+++ frontend/src/components/Trials/Instruction/InstructionContent.ts
@@ -1,23 +1,30 @@
 const instructions = {
-    welcome: [""],
-    practice_rounds: ["Well done! Next, you will practice  with two more complex networks. Try to maximize the number of points in each round, just as you want to do in the main task where they count towards your bonus payment!"],
-    written_strategy_start: ["Please think about how you approached the task and write down the strategy you used in trying to maximize points."],
-    learning_selection: [
-        "Before you start with the main task, you have the opportunity to learn from the solutions submitted by previous players.",
-        "Note that your solutions in the main task might also be presented to future players so that they can learn from them."],
-    pre_social_learning: [
-        "You will first see a demonstration by the selected player. You then have to solve the network from the demonstration yourself. You can earn points by correctly repeating the moves demonstrated by the selected player. These points will be converted into a bonus payment for you.",
-        "Afterwards, you have to solve the network by yourself. This time, the number of points you earn will depend not on correctly copying the selected players move, but solely on the moves you choose to make (they could be the same as the ones demonstrated by the selected player, or different). These points will also be converted into a bonus payment for you."],
-    pre_social_learning_gen0: [
-        "Now the main task begins. You will have to navigate through four different networks.",
-        "All points you collect will be converted into your bonus payment. Try to maximize your points!"],
-    demonstration: [
-        "Well done! You now enter the final part of the experiment. Now you will play four more rounds that serve as demonstrations for future players to learn from. You will not earn points directly, but the second half of your bonus payment depends on the performance of people that learn from you, so try your best!"],
-    written_strategy: ["Please write down your strategy in solving the task."],
-    consent_decline: ["You have declined to participate in the experiment. Thank you for your time. Please return the study on Prolific."],
+  welcome: [""],
+  practice_rounds: [
+    "Well done! Next, you will practice  with two more complex networks. Try to maximize the number of points in each round, just as you want to do in the main task where they count towards your bonus payment!",
+  ],
+  written_strategy_start: [
+    "Please think about how you approached the task and write down the strategy you used in trying to maximize points.",
+  ],
+  learning_selection: [
+    "Before you start with the main task, you have the opportunity to learn from the solutions submitted by previous players.",
+    "Note that your solutions in the main task might also be presented to future players so that they can learn from them.",
+  ],
+  pre_social_learning: [
+    "You will first see a demonstration by the selected player. You then have to solve the network from the demonstration yourself. You can earn points by correctly repeating the moves demonstrated by the selected player. These points will be converted into a bonus payment for you.",
+    "Afterwards, you have to solve the network by yourself. This time, the number of points you earn will depend not on correctly copying the selected players move, but solely on the moves you choose to make (they could be the same as the ones demonstrated by the selected player, or different). These points will also be converted into a bonus payment for you.",
+  ],
+  pre_social_learning_gen0: [
+    "Now the main task begins. You will have to navigate through four different networks.",
+    "All points you collect will be converted into your bonus payment. Try to maximize your points!",
+  ],
+  demonstration: [
+    "Well done! You now enter the final part of the experiment. Now you will play four more rounds that serve as demonstrations for future players to learn from. Another part of your bonus payment depends on the performance of people that learn from you, so try your best!",
+  ],
+  written_strategy: ["Please write down your strategy in solving the task."],
+  consent_decline: [
+    "You have declined to participate in the experiment. Thank you for your time. Please return the study on Prolific.",
+  ],
 };
 
-
 export default instructions;
-
-
